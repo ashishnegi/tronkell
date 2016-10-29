1fd14034f16edd07fd092df37068c1b65dcafb66
Ashish Negi <ashish@quintype.com>
2016-10-29 20:41:12 +0530

    Moves common arbitrary functions to TestUtils.hs

--- test/TestEngine.hs
+++ test/TestEngine.hs
@@ -1,78 +1,18 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE RecordWildCards #-}
 
 module TestEngine where
 
+import TestUtils
 import Tronkell.Types
 import Tronkell.Game.Types
 import Tronkell.Game.Engine
 
 import qualified Data.Map as Map
-import Data.List (nub, nubBy)
+import Data.List (nub)
 import Data.Maybe (isNothing, fromJust)
-import Data.String (fromString)
-import System.Random
 
 import Test.Hspec
 import Test.QuickCheck
-
-newtype BoundedInt = BoundedInt { unBoundedInt :: Int }
-                     deriving (Show, Eq, Ord, Num)
-
-instance Random BoundedInt where
-  randomR (BoundedInt lo, BoundedInt hi) g =
-    let (a, g') = randomR (lo, hi) g in (BoundedInt a, g')
-
-  random g = let (a, g') = random g in (BoundedInt a, g')
-
-instance Arbitrary BoundedInt where
-  arbitrary = BoundedInt <$> choose (0, 100)
-
-instance Arbitrary GameConfig where
-  arbitrary = do
-    w  <- unBoundedInt <$> arbitrary `suchThat` (> 0)
-    h  <- unBoundedInt <$> arbitrary `suchThat` (> 0)
-    sp <- unBoundedInt <$> arbitrary
-    return $ GameConfig w h sp 1
-
-instance Arbitrary Player where
-  arbitrary = do
-    i <- PlayerId <$> arbitrary
-    n <- PlayerNick . fromString <$> arbitrary
-    x <- unBoundedInt <$> arbitrary
-    y <- unBoundedInt <$> arbitrary
-    o <- arbitraryBoundedEnum
-    return $ Player i n Alive (x, y) o []
-
-isValidPlayer :: GameConfig -> Player -> Bool
-isValidPlayer (GameConfig w h _ _) (Player _ _ _ (x, y) _ _) = x < w && y < h
-
-areOverlappingPlayers :: Player -> Player -> Bool
-areOverlappingPlayers p1 p2 =
-  playerNick p1 == playerNick p2 || playerCoordinate p1 == playerCoordinate p2
-
-genPlayer :: GameConfig -> Gen Player
-genPlayer GameConfig{..} = do
-  pid  <- PlayerId <$> arbitrary
-  nick <- PlayerNick . fromString <$> arbitrary
-  x    <- unBoundedInt <$> arbitrary `suchThat` (>= 0) `suchThat` (< BoundedInt gameWidth)
-  y    <- unBoundedInt <$> arbitrary `suchThat` (>= 0) `suchThat` (< BoundedInt gameHeight)
-  o    <- arbitraryBoundedEnum
-  return $ Player pid nick Alive (x, y) o []
-
-instance Arbitrary Game where
-  arbitrary = do
-    conf   <- arbitrary
-    np     <- unBoundedInt
-              <$> arbitrary `suchThat` (< (BoundedInt $ gameWidth conf * gameHeight conf))
-                            `suchThat` (> 0)
-    ps     <- nubBy areOverlappingPlayers
-              <$> vectorOf np (genPlayer conf)
-    let mp = Map.fromList . map (\p -> (playerId p, p)) $ ps
-    return $ Game Nothing mp InProgress conf
-
-isPlayerOnGrid :: Game -> Player -> Bool
-isPlayerOnGrid game = isValidPlayer (gameConfig game)
 
 main :: IO ()
 main = hspec $ do
--- test/TestServer.hs
+++ test/TestServer.hs
@@ -9,7 +9,6 @@ import Test.QuickCheck.Monadic
 
 import Data.ByteString.Char8 as C (pack)
 import qualified Data.Map as M
-import System.IO
 import Control.Concurrent
 import Control.Concurrent.STM
 
@@ -17,19 +16,6 @@ import Tronkell.Server.Types as STypes
 import Tronkell.Server.Server as SServer
 import Tronkell.Game.Types as GTypes
 
-instance Show Server where
-  show Server{..} = "Server: " ++ show serverGameConfig
-
-genServer = do
-    let gConf = GTypes.GameConfig 100 100 1 1
-        sConf = STypes.ServerConfig 4242
-    users <- newMVar M.empty
-    networkInChan         <- newChan
-    clientSpecificOutChan <- newChan
-    serverChan <- atomically newTChan
-    clientsChan <- newChan
-    internalChan <- newChan
-    return $ Server gConf sConf users (networkInChan, clientSpecificOutChan) serverChan clientsChan internalChan
 
 -- rethink the tests.
 main :: IO ()
